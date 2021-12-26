0e8f521501668066f3d8f247fd6d3e64e184430b
Gladioluss <74546538+Gladioluss@users.noreply.github.com>
2021-12-26 23:24:46 +0500

    Update SQLTask.java

--- UlearnProject/src/main/java/com/gladiolus/SQLTask.java
+++ UlearnProject/src/main/java/com/gladiolus/SQLTask.java
@@ -18,7 +18,7 @@ public class SQLTask {
             for (var e : counties)
                 statement.setObject(1, e);
 
-            System.out.println(statement.executeQuery().getInt(1));
+            System.out.printf("Среднее количество расходов в Fresno, Contra Costa, El Dorado и Glenn, у которых расход больше 10 : %s%n",statement.executeQuery().getInt(1));
         } catch (SQLException e) {
             throw new SQLException();
         }
@@ -26,7 +26,7 @@ public class SQLTask {
 
     public static void displaySchoolWithMaxMath(Connection connection) throws SQLException {
         try (var statement = connection.prepareStatement("SELECT school FROM schools WHERE 5000 < students < 7500 OR 10000 < students < 11000 ORDER BY math DESC LIMIT 1")) {
-            System.out.println(statement.executeQuery().getString(1));
+            System.out.printf("Учебное заведение, с количеством студентов равному от 5000 до 7500 и с 10000 до 11000, с самым высоким показателем по математике : %s%n",statement.executeQuery().getString(1));
         } catch (SQLException e) {
             throw new SQLException();
         }
